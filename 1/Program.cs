266cb5313bc860e17f0c6357672c69c94af995c6
German <german.glumov@gmail.com>
2022-12-19 17:27:48 +0300

    один

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> numbers = new List<int>();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < m; i++) 
+{
+    numbers.Add(Convert.ToInt32(Console.ReadLine()));
+    if(numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write("[{0}]", string.Join(", ", numbers));
+Console.WriteLine($" --> {count}");
